7cbdc8bc5446c15e80e9879b38861a57ed29a7c2
yujoy <me@joyyu.xyz>
2021-07-14 15:25:45 -0400

    init commit

---
+++ tap_megaphone/tap.py
@@ -0,0 +1,36 @@
+"""megaphone tap class."""
+
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_megaphone.streams import (
+    megaphoneStream,
+    UsersStream,
+    GroupsStream,
+)
+# TODO: Compile a list of custom stream types here
+#       OR rewrite discover_streams() below with your custom logic.
+STREAM_TYPES = [
+    UsersStream,
+    GroupsStream,
+]
+
+
+class Tapmegaphone(Tap):
+    """megaphone tap class."""
+    name = "tap-megaphone"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property("auth_token", th.StringType, required=True),
+        th.Property("project_ids", th.ArrayType(th.StringType), required=True),
+        th.Property("start_date", th.DateTimeType),
+        th.Property("api_url", th.StringType, default="https://api.mysample.com"),
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
